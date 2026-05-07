33541154aca6419a58146d78593bf2643d24d2f7
OneMinorBug <zhibosheng12@gmail.com>
2026-05-07 19:23:32 -0400

    Added Multiple.java, Reduced.java and checked more boxes in README

---
+++ src/Multiple.java
@@ -0,0 +1,10 @@
+public class Multiple {
+
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            if (i % 3 == 0 || i % 5 == 0) count++;
+        }
+        System.out.printf("There are %d multiples of 3 or 5 below 1000", count);
+    }
+}
